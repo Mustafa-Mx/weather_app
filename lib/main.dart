f6698145f9b0cab6076fd4e1c8b37273f62c6fed
“Mustafa-Mxgit config --global user.email “m.rocky34511@gmail.com <m.rockay34511@gmail.com>
2024-03-25 00:59:28 +0500

    New UI based on Source 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/homescreen.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -10,26 +11,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Weather Forecaster",
-            style: TextStyle(color: Colors.purpleAccent),
-          ),
-          backgroundColor: Colors.white,
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/night.png"),
-                  fit: BoxFit.fill)),
-          child: const Center(
-              child: Text(
-            "Welcome to Weather Forecaster",
-            style: TextStyle(color: Colors.white),
-          )),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Weather Forecaster",
+            style: TextStyle(color: Colors.purpleAccent),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/night.png"),
+                    fit: BoxFit.fill)),
+            child: Center(
+              child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(44)),
+                      color: Colors.black45),
+                  child: Text(
+                    "Welcome to Weather Forecaster",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  )),
+            )),
+      ),
+    );
+  }
+}
